843e59678150f3d717aadd278194662d09f6f59a
Dishant Gandhi <37023998+dish34@users.noreply.github.com>
2024-02-12 03:02:18 +0530

    First Commit for Anime Store

---
+++ src/main/java/com/app/animestore/AnimeStore/AnimeStoreApplication.java
@@ -0,0 +1,13 @@
+package com.app.animestore.AnimeStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimeStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimeStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/animestore/AnimeStore/AnimeStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.animestore.AnimeStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
